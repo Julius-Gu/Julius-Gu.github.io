28b35935dcb412d199a453a808133043a6410cce
Julius Guthunz <57110140+Julius-Gu@users.noreply.github.com>
2021-06-12 01:57:44 +0200

    Add files via upload

---
+++ cv/script.js
@@ -0,0 +1,43 @@
+tof = document.getElementById("table_of_contents");
+Array.from(document.getElementsByClassName("heading")).forEach( header => {
+    txt = header.innerText;
+    header.innerHTML = "<a href=\"#"+txt+"\">"+
+        txt+
+        "<image class=\"icon ico_link\" src=\"icons/light_link.svg\"></image></a>";
+    header.id = txt;
+
+    tof_element = '<a class="'+header.nodeName+'" href="\#'+
+            txt+'\"><image class="icon ico_goto" src="icons/light_goto.svg"></image> '+
+            txt+
+            '</a><br>';
+
+    tof.innerHTML += tof_element;
+    console.log(tof_element);
+});
+
+var dark_theme = false;
+document.documentElement.setAttribute('data-theme', 'light');
+function switch_theme() {
+    if(dark_theme) {
+        document.documentElement.setAttribute('data-theme', 'light');
+        document.getElementById("ico_theme").src = "icons/light_theme.svg";
+        Array.from(document.getElementsByClassName("ico_link")).forEach( icon => {
+            icon.src="icons/light_link.svg";
+        });
+        Array.from(document.getElementsByClassName("ico_goto")).forEach( icon => {
+            icon.src="icons/light_goto.svg";
+        });
+    } else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        document.getElementById("ico_theme").src = "icons/dark_theme.svg";
+        Array.from(document.getElementsByClassName("ico_link")).forEach( icon => {
+            icon.src="icons/dark_link.svg";
+        });
+        Array.from(document.getElementsByClassName("ico_goto")).forEach( icon => {
+            icon.src="icons/dark_goto.svg";
+        });
+    }
+    dark_theme = !dark_theme;
+}
+
+console.log("hello world");
